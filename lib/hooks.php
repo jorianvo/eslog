dab89f9bb907fab8e43692d055595fc356ca38a7
jorianvo <jovofdt@gmail.com>
2015-06-12 13:16:58 +0200

    Create a new function to run when hooked

--- lib/hooks.php
+++ lib/hooks.php
@@ -13,11 +13,14 @@ class OC_esLog_Hooks {
 	// For now just reading and writing data
 	// ---------------------
 
+  // This function is called every time a file is read
 	static public function read($path) {
-		OC_esLog::log($path,'File read');
+		OC_esLog::sendToStatsd($path,'File read');
 	}
+
+	// This function is called every time a file is written
 	static public function write($path) {
-		OC_esLog::log($path,'File write');
+		OC_esLog::sendToStatsd($path,'File write');
 	}
 
 }

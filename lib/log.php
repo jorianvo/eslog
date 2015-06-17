8125a85cf2a1dfc1b55655c06cff9cc477703e6b
jorianvo <jovofdt@gmail.com>
2015-06-17 16:13:21 +0200

    Replace all tab characters with two spaces

--- lib/log.php
+++ lib/log.php
@@ -8,22 +8,22 @@ use GeoIp2\Database\Reader;
 
 class OC_esLog {
 
-	public function __construct(){
+  public function __construct(){
 
-	}
+  }
 
-	// This function will gather the stats and send them statsd
-	// The path is the path of the file that is read or written
-	// currently this is not used. The action is either 'File read'
-	// or 'File write' and determines what value to send to statsd
-	public static function sendToStatsd($path,$action){    
-		// Create the udp socket to send the data to statsd
-		$sender = new SocketSender('localhost', 8125, 'udp');
+  // This function will gather the stats and send them statsd
+  // The path is the path of the file that is read or written
+  // currently this is not used. The action is either 'File read'
+  // or 'File write' and determines what value to send to statsd
+  public static function sendToStatsd($path,$action){
+    // Create the udp socket to send the data to statsd
+    $sender = new SocketSender('localhost', 8125, 'udp');
 
-		// Create the client which sends to data to statsd using the sender
-		$client  = new StatsdClient($sender);
-		$factory = new StatsdDataFactory('\Liuggio\StatsdClient\Entity\StatsdData');
-		$service = new StatsdService($client, $factory);
+    // Create the client which sends to data to statsd using the sender
+    $client  = new StatsdClient($sender);
+    $factory = new StatsdDataFactory('\Liuggio\StatsdClient\Entity\StatsdData');
+    $service = new StatsdService($client, $factory);
 
     // Get ip and city of origin
     //if(isset($_SERVER["REMOTE_ADDR"])) {
@@ -40,16 +40,16 @@ class OC_esLog {
 
     //throw new \Exception("country of origin = $country");
 
-		// Check if file is read or written
-		if ($action == "File read"){
-			$service->increment('read');
-		} elseif ($action == "File write"){
+    // Check if file is read or written
+    if ($action == "File read"){
+      $service->increment('read');
+    } elseif ($action == "File write"){
       $service->increment("files.writes.".$country);
-		}
+    }
 
-		// Send the data over the socket to statsd
-		$service->flush();
-	}
+    // Send the data over the socket to statsd
+    $service->flush();
+  }
 
   // This function will return the corresponding country given a city
   public static function IpToCountry($ip) {

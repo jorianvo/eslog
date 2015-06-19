8a0247e00232dca6993a8070d40ca46136ea372e
jorianvo <jovofdt@gmail.com>
2015-06-19 17:52:05 +0200

    Update template to create fields form stated i.e. host and port

--- settings.php
+++ settings.php
@@ -1,10 +1,6 @@
 <?php
 $tmpl = new OC_Template('eslog', 'settings');
-$tmpl->assign('eslog_host', OC_Appconfig::getValue('eslog', 'eslog_host','127.0.0.1:9200'));
-$tmpl->assign('eslog_auth', OC_Appconfig::getValue('eslog', 'eslog_auth','none'));
-$tmpl->assign('eslog_user', OC_Appconfig::getValue('eslog', 'eslog_user',''));
-$tmpl->assign('eslog_password', OC_Appconfig::getValue('eslog', 'eslog_password',''));
-$tmpl->assign('eslog_index', OC_Appconfig::getValue('eslog', 'eslog_index','owncloud'));
-$tmpl->assign('eslog_type', OC_Appconfig::getValue('eslog', 'eslog_type','owncloud'));
+$tmpl->assign('eslog_host', OC_Appconfig::getValue('eslog', 'eslog_host','127.0.0.1'));
+$tmpl->assign('eslog_auth', OC_Appconfig::getValue('eslog', 'eslog_port','8125'));
 
 return $tmpl->fetchPage();

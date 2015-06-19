3508470b8467fc369740e1730f78e8c558ee8c5a
jorianvo <jovofdt@gmail.com>
2015-06-19 15:11:01 +0200

    Add function to hook into webdav actions

--- lib/hooks.php
+++ lib/hooks.php
@@ -7,20 +7,40 @@ require_once 'apps/eslog/lib/log.php';
 
 class OC_esLog_Hooks {
 
-	// ---------------------
-	// Filesystem operations
-	//
-	// For now just reading and writing data
-	// ---------------------
+  // ---------------------
+  // Filesystem operations
+  //
+  // For now just reading and writing data
+  // ---------------------
 
   // This function is called every time a file is read
-	static public function read($path) {
-		OC_esLog::sendToStatsd($path,'File read');
-	}
+  static public function read($path) {
+    OC_esLog::sendToStatsd($path,'File read');
+  }
 
-	// This function is called every time a file is written
-	static public function write($path) {
-		OC_esLog::sendToStatsd($path,'File write');
-	}
+  // This function is called every time a file is written
+  static public function write($path) {
+    OC_esLog::sendToStatsd($path,'File write');
+  }
 
+  // This function is called every time a file is written using webdav
+  static public function webdav($vars) {
+    // Get protocal
+    if(isset($vars['SCRIPT_NAME']) && basename($vars['SCRIPT_NAME'])=='remote.php'){
+      $paths=explode('/',$vars['REQUEST_URI']);
+      $pos=array_search('remote.php',$paths);
+      $protocol=$paths[$pos+1];
+    }
+
+    // Get request method
+    $action=strtolower($vars['REQUEST_METHOD']);
+
+    // If using webdav i.e. desktop or mobile sync client
+    // Get the request method
+    if($protocol=='webdav'){
+      if ($action=='put') {
+        OC_esLog::sendToStatsd(NULL,'File write');
+      }
+    }
+  }
 }

c67d3803473d94b56454972e4a0ebd0eaa07e3b7
jorianvo <jovofdt@gmail.com>
2015-06-19 18:11:24 +0200

    Fix wrong assignment and add protocol to use to communicate with statsd

--- settings.php
+++ settings.php
@@ -1,6 +1,7 @@
 <?php
 $tmpl = new OC_Template('eslog', 'settings');
 $tmpl->assign('eslog_host', OC_Appconfig::getValue('eslog', 'eslog_host','127.0.0.1'));
-$tmpl->assign('eslog_auth', OC_Appconfig::getValue('eslog', 'eslog_port','8125'));
+$tmpl->assign('eslog_port', OC_Appconfig::getValue('eslog', 'eslog_port','8125'));
+$tmpl->assign('eslog_proto', OC_Appconfig::getValue('eslog', 'eslog_proto','udp'));
 
 return $tmpl->fetchPage();
